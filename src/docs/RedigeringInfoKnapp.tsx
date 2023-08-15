35fd1c2fc538de889c0b98b49dac60563a1eed73
Ugur Alpay Cenar <cenar93@gmail.com>
2023-08-15 09:00:17 +0200

    Styling fix sidebar and infobutton (#19)

--- src/docs/RedigeringInfoKnapp.tsx
+++ src/docs/RedigeringInfoKnapp.tsx
@@ -4,8 +4,12 @@ import EditorHelpDocs from "./EditorHelpDocs.mdx";
 import InfoKnapp from "./InfoKnapp";
 export default function RedigeringInfoKnapp() {
     return (
-        <div className="redigering-info-knapp border-subtle">
-            <InfoKnapp className="redigering-info-content">
+        <div className="z-50 agroup fixed bottom-4 right-8 p-2 flex items-end justify-end w-24 h-24">
+            <InfoKnapp
+                className="redigering-info-content"
+                buttonText="Brukerveiledning"
+                buttonClassName="border rounded-xl border-solid bg-white"
+            >
                 <EditorHelpDocs />
             </InfoKnapp>
         </div>
