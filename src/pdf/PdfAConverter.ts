2174bede0b715f611e44ed6bc30845c33b96bfa8
Ugur Cenar <cenar93@gmail.com>
2023-10-26 19:08:15 +0200

    Do not add documentid to pdf

--- src/pdf/PdfAConverter.ts
+++ src/pdf/PdfAConverter.ts
@@ -24,12 +24,11 @@ export class PdfAConverter {
         const documentId = crypto.randomUUID();
         this.flattenForm(pdfDoc);
         this.addMetadata(origDoc, pdfDoc, documentDate, documentId, title);
-        this.addDocumentId(pdfDoc, documentId);
         this.removeXFA(pdfDoc);
         await this.addFont(pdfDoc);
         this.addColorProfile(pdfDoc);
         this.deleteJavascript(pdfDoc);
-        return await pdfDoc.save({
+        return pdfDoc.save({
             useObjectStreams: false,
         });
     }
@@ -138,40 +137,40 @@ export class PdfAConverter {
         pdfDoc.setModificationDate(date);
         const metadataXML = `
         <?xpacket begin="" id="${documentId}"?>
-          <x:xmpmeta xmlns:x="adobe:ns:meta/" x:xmptk="Adobe XMP Core 5.2-c001 63.139439, 2010/09/27-13:37:26">
-            <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
-    
-              <rdf:Description rdf:about="" xmlns:dc="http://purl.org/dc/elements/1.1/">
-                <dc:format>application/pdf</dc:format>
-                <dc:creator>
-                  <rdf:Seq>
-                    <rdf:li>${author}</rdf:li>
-                  </rdf:Seq>
-                </dc:creator>
-                <dc:title>
-                   <rdf:Alt>
-                      <rdf:li xml:lang="x-default">${title}</rdf:li>
-                   </rdf:Alt>
-                </dc:title>
-              </rdf:Description>
-    
-              <rdf:Description rdf:about="" xmlns:xmp="http://ns.adobe.com/xap/1.0/">
-                <xmp:CreatorTool>${creator}</xmp:CreatorTool>
-                <xmp:CreateDate>${this._formatDate(originalCreationDate ?? date)}</xmp:CreateDate>
-                <xmp:ModifyDate>${this._formatDate(date)}</xmp:ModifyDate>
-                <xmp:MetadataDate>${this._formatDate(date)}</xmp:MetadataDate>
-              </rdf:Description>
-    
-              <rdf:Description rdf:about="" xmlns:pdf="http://ns.adobe.com/pdf/1.3/">
-                <pdf:Producer>${producer}</pdf:Producer>
-              </rdf:Description>
-    
-              <rdf:Description rdf:about="" xmlns:pdfaid="http://www.aiim.org/pdfa/ns/id/">
-                <pdfaid:part>1</pdfaid:part>
-                <pdfaid:conformance>B</pdfaid:conformance>
-              </rdf:Description>
-            </rdf:RDF>
-          </x:xmpmeta>
+        <x:xmpmeta xmlns:x="adobe:ns:meta/" x:xmptk="Adobe XMP Core 5.2-c001 63.139439, 2010/09/27-13:37:26">
+        <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+
+        <rdf:Description rdf:about="" xmlns:dc="http://purl.org/dc/elements/1.1/">
+        <dc:format>application/pdf</dc:format>
+        <dc:creator>
+        <rdf:Seq>
+        <rdf:li>${author}</rdf:li>
+        </rdf:Seq>
+        </dc:creator>
+        <dc:title>
+        <rdf:Alt>
+        <rdf:li xml:lang="x-default">${title}</rdf:li>
+        </rdf:Alt>
+        </dc:title>
+        </rdf:Description>
+
+        <rdf:Description rdf:about="" xmlns:xmp="http://ns.adobe.com/xap/1.0/">
+        <xmp:CreatorTool>${creator}</xmp:CreatorTool>
+        <xmp:CreateDate>${this._formatDate(originalCreationDate ?? date)}</xmp:CreateDate>
+        <xmp:ModifyDate>${this._formatDate(date)}</xmp:ModifyDate>
+        <xmp:MetadataDate>${this._formatDate(date)}</xmp:MetadataDate>
+        </rdf:Description>
+
+        <rdf:Description rdf:about="" xmlns:pdf="http://ns.adobe.com/pdf/1.3/">
+        <pdf:Producer>${producer}</pdf:Producer>
+        </rdf:Description>
+
+        <rdf:Description rdf:about="" xmlns:pdfaid="http://www.aiim.org/pdfa/ns/id/">
+        <pdfaid:part>1</pdfaid:part>
+        <pdfaid:conformance>B</pdfaid:conformance>
+        </rdf:Description>
+        </rdf:RDF>
+        </x:xmpmeta>
         <?xpacket end="w"?>
         `.trim();
         const metadataStream = pdfDoc.context.stream(metadataXML, {
