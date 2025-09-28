0dd1491e57b7166269ce99be697042bcc4badcd8
Ugur Alpay Cenar <cenar93@gmail.com>
2025-09-28 06:06:18 +0200

    Fiks byggfeil

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,6 @@ import { useApi } from "@navikt/bidrag-ui-common";
 import environment from "../environment";
 import { Api as BidragDokumentApi } from "./BidragDokumentApi";
 import { Api as BidragForsendelseApi } from "./BidragDokumentForsendelseApi";
-import { Api as BidragDokumentProduksjonApi } from "./BidragDokumentProduksjonApi";
 
 export const BIDRAG_DOKUMENT_API = useApi(new BidragDokumentApi({ baseURL: environment.url.bidragDokument }), {
     app: "bidrag-dokument",
@@ -13,14 +12,6 @@ export const BIDRAG_FORSENDELSE_API = useApi(
     new BidragForsendelseApi({ baseURL: environment.url.bidragDokumentForsendelse }),
     {
         app: "bidrag-dokument-forsendelse",
-        cluster: "gcp",
-    }
-);
-
-export const BIDRAG_DOKUMENT_PRODUKSJON_API = useApi(
-    new BidragDokumentProduksjonApi({ baseURL: environment.url.bidragDokumentProduksjon }),
-    {
-        app: "bidrag-dokument-produksjon",
         cluster: "gcp",
     }
 );
