9e7e417ffe908d6868b6fbf5009dca5df0050e54
Ugur Alpay Cenar <cenar93@gmail.com>
2025-09-28 05:53:54 +0200

    Oppgradere felles

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,14 +3,24 @@ import { useApi } from "@navikt/bidrag-ui-common";
 import environment from "../environment";
 import { Api as BidragDokumentApi } from "./BidragDokumentApi";
 import { Api as BidragForsendelseApi } from "./BidragDokumentForsendelseApi";
+import { Api as BidragDokumentProduksjonApi } from "./BidragDokumentProduksjonApi";
 
-export const BIDRAG_DOKUMENT_API = useApi(
-    new BidragDokumentApi({ baseURL: environment.url.bidragDokument }),
-    "bidrag-dokument",
-    "fss"
-);
+export const BIDRAG_DOKUMENT_API = useApi(new BidragDokumentApi({ baseURL: environment.url.bidragDokument }), {
+    app: "bidrag-dokument",
+    cluster: "fss",
+});
 export const BIDRAG_FORSENDELSE_API = useApi(
     new BidragForsendelseApi({ baseURL: environment.url.bidragDokumentForsendelse }),
-    "bidrag-dokument-forsendelse",
-    "gcp"
+    {
+        app: "bidrag-dokument-forsendelse",
+        cluster: "gcp",
+    }
+);
+
+export const BIDRAG_DOKUMENT_PRODUKSJON_API = useApi(
+    new BidragDokumentProduksjonApi({ baseURL: environment.url.bidragDokumentProduksjon }),
+    {
+        app: "bidrag-dokument-produksjon",
+        cluster: "gcp",
+    }
 );
